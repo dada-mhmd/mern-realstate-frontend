545dc4d70aed09659a5285cf005493d85a52e158
dada-mhmd <moudi.ld@hotmail.com>
2023-09-26 10:25:21 +0300

    update the header and make private routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { About, Home, Profile, SignIn, SignUp } from './pages'
 import { Header } from './components'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -11,8 +12,11 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/profile' element={<Profile />} />
         <Route path='/about' element={<About />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
