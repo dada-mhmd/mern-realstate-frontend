05d603b55335885db38af73d75483656eb380faf
dada-mhmd <moudi.ld@hotmail.com>
2023-10-02 09:15:30 +0300

    all done

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,27 @@
 const About = () => {
-  return <div>About</div>
-}
+  return (
+    <main className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Real Estate
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis
+        vitae cumque architecto, voluptas aperiam temporibus. Dolorum, totam.
+        Saepe vero dicta, expedita rem voluptate magnam ipsam sed voluptatem ex
+        beatae porro nisi minima, modi omnis quaerat recusandae sint aliquid
+        magni neque? Fugiat natus iure animi? Sit, soluta! Necessitatibus
+        tenetur dolore commodi!
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
+        fuga cupiditate iste quo, saepe repudiandae corporis. Nulla nisi
+        accusamus non earum quidem nostrum similique a dicta consectetur
+        perspiciatis aliquam porro aspernatur voluptatum, architecto vero,
+        laudantium, eveniet reiciendis aperiam molestiae fuga! Eius ea est nulla
+        corrupti nihil. Sint aliquam iusto non?
+      </p>
+    </main>
+  );
+};
 
-export default About
+export default About;
