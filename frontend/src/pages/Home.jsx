f51f1b14cc823c0e527f88e515bb8e4b0f769de4
dada-mhmd <moudi.ld@hotmail.com>
2023-10-02 09:12:24 +0300

    home page completed.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,150 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/bundle';
+import SwiperCore from 'swiper';
+import { Navigation } from 'swiper/modules';
+import ListingsCard from '../components/ListingsCard';
 const Home = () => {
-  return <div>Home</div>
-}
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
 
-export default Home
+  SwiperCore.use([Navigation]);
+
+  useEffect(() => {
+    const fetchOfferListings = async () => {
+      try {
+        const res = await fetch(`/api/listing/get?offer=true&limit=4`);
+        const data = await res.json();
+        setOfferListings(data);
+        fetchRentListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchRentListings = async () => {
+      try {
+        const res = await fetch(`/api/listing/get?type=rent&limit=4`);
+        const data = await res.json();
+        setRentListings(data);
+        fetchSaleListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchSaleListings = async () => {
+      try {
+        const res = await fetch(`/api/listing/get?type=sale&limit=4`);
+        const data = await res.json();
+        setSaleListings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchOfferListings();
+  }, []);
+
+  return (
+    <main>
+      {/* top */}
+      <section className='flex flex-col gap-6 py-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+          Find your next
+          <span className='text-slate-500'> perfect </span>
+          <br /> place with ease
+        </h1>
+        <div className='text-gray-400 text-xs sm:text-sm'>
+          RealEstate is the best place to find your next perfect place to live.
+          <br />
+          We have a wide range of properties for you to choose.
+        </div>
+        <Link
+          className='text-xs sm:text-sm text-white font-bold w-fit bg-zinc-700 border-none p-3 rounded hover:opacity-95 uppercase'
+          to={'/search'}
+        >
+          Let's get started
+        </Link>
+      </section>
+
+      {/* swiper */}
+      <Swiper navigation>
+        {offerListings &&
+          offerListings.length > 0 &&
+          offerListings?.map((listing) => (
+            <SwiperSlide key={listing._id}>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+                className='h-[500px]'
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      {/* listing results */}
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+        {offerListings && offerListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>
+                Recent Offers
+              </h2>
+              <Link to={'/search?offer=true'} className='text-sm text-blue-800'>
+                Show more offers
+              </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {offerListings?.map((listing) => (
+                <ListingsCard key={listing._id} listing={listing} />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {rentListings && rentListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>
+                Recent places for rent
+              </h2>
+              <Link to={'/search?type=rent'} className='text-sm text-blue-800'>
+                Show more places for rent
+              </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {rentListings?.map((listing) => (
+                <ListingsCard key={listing._id} listing={listing} />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {saleListings && saleListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>
+                Recent places for sale
+              </h2>
+              <Link to={'/search?type=sale'} className='text-sm text-blue-800'>
+                Show more places for sale
+              </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {saleListings?.map((listing) => (
+                <ListingsCard key={listing._id} listing={listing} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
